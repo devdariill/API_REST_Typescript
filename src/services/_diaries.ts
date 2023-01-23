492ef765710dad459d6a20e5a8d3592a212b9bb9
nATOr <gowluonade@gmail.com>
2023-01-23 08:14:43 -0500

    fix error enum types.d.ts

--- src/services/_diaries.ts
+++ src/services/_diaries.ts
@@ -1,32 +1,32 @@
-import { DiaryEntry } from '../types'
+import { DiaryEntry, Visibility, Weather } from '../types'
 
 const diariEntries: DiaryEntry[] = [
   {
     id: 1,
     date: '2017-01-01',
-    weather: 'rainy',
-    visibility: 'poor',
+    weather: Weather.Sunny,
+    visibility: Visibility.Good,
     comment: "Pretty scary flight, I'm glad I'm alive"
   },
   {
     id: 2,
     date: '2017-04-01',
-    weather: 'sunny',
-    visibility: 'good',
+    weather: Weather.Cloudy,
+    visibility: Visibility.Average,
     comment: "Everything went better than expected, I'm learning much"
   },
   {
     id: 3,
     date: '2017-04-15',
-    weather: 'windy',
-    visibility: 'good',
+    weather: Weather.Windy,
+    visibility: Visibility.Good,
     comment: "I'm getting pretty confident although I hit a flock of birds"
   },
   {
     id: 4,
     date: '2017-05-11',
-    weather: 'cloudy',
-    visibility: 'good',
+    weather: Weather.Cloudy,
+    visibility: Visibility.Poor,
     comment: 'I almost failed the landing but I survived'
   }
 ]
