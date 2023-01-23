5abe318fcdcac7828979f05bca08fa850f528c3f
nATOr <gowluonade@gmail.com>
2023-01-23 06:57:41 -0500

    by id

--- src/routes/diaries.routes.ts
+++ src/routes/diaries.routes.ts
@@ -1,10 +1,15 @@
 import express from 'express'
-import * as diariesService from '../services/diaryServices'
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router()
 
 router.get('/', (_, res) => {
-  res.send(diariesService.getEntriesWithoutSesitiveInfo())
+  res.send(diaryServices.getEntriesWithoutSesitiveInfo())
+})
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.fintById(+req.params.id)
+  return diary != null ? res.send(diary) : res.sendStatus(404)
+  //   res.send(diary?.comment)
 })
 router.post('/', (_, res) => {
   res.send('Saving a diary')
